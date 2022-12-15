74dfe952daf8f6de931a6e784a008ea31922fb98
syamjithbabu <syamjithbabu2000@gmail.com>
2022-12-15 06:12:59 -0800

    v1

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn papercorner.wsgi --log-file -
--- papercorner/settings.py
+++ papercorner/settings.py
@@ -122,6 +122,7 @@ STATIC_URL = '/static/'
 STATIC_FILE_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = ((BASE_DIR / 'static'),)
 STATIC_ROOT = BASE_DIR / 'assets'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
